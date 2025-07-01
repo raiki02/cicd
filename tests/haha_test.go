8bb768fce62feda38a2a8937efe18e37555b8064
raiki02 <raikisakura@qq.com>
2025-07-01 18:30:04 +0800

    故意错误test

---
+++ tests/haha_test.go
@@ -0,0 +1,19 @@
+package tests
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	type testCase struct {
+		a, b int
+	}
+
+	a := 1
+	b := 2
+	want := 5
+
+	got := Add(a, b)
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+
+}
